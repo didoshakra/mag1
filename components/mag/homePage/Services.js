30c262c8cf9aea03653d2d34aa9ee090ecc8e28e
Roman <didoshakr@ukr.net>
2020-05-28 14:29:06 +0300

    а

---
+++ components/mag/homePage/Services.js
@@ -0,0 +1,189 @@
+//Services.js / Muiv4.5.1
+//Добавлено animat
+import React, { useContext } from "react";
+// import useTranslation from "../../../translations/useTranslation";
+// import { ComponentContext } from "../../../context/ComponentContext";
+//import IconFreeShiping from "../../ui/icons/IconFreeShiping";
+import IconFreeShipingBant from "../../ui/icons/IconFreeShipingBant";
+import IconVegetable from "../../ui/icons/IconVegetable";
+import IconSupport from "../../ui/icons/IconSupport";
+// import IconHightQuality from "../../ui/icons/IconHightQuality";
+import IconSuperiorQuality from "../../ui/icons/IconSuperiorQuality";
+
+const Services = () => {
+  // export default function HomePage() {
+  // const { t } = useTranslation();
+  // const { state } = useContext(ComponentContext);
+  // const theme = state.theme;
+
+  return (
+    <section className="servises__section">
+      {/* Елемент з іконкою */}
+      <div className="servises__item">
+        <div className="servises__item__icon bg-color-1">
+          <div className="servises__item__icon__circle ">
+            <IconFreeShipingBant
+              width="50"
+              height="50"
+              color="#fff"
+              color1="#fff"
+            />
+          </div>
+        </div>
+        <div className="servises__item__body">
+          <h3 className="servises__item__body__heading">Free Shipping</h3>
+          <span>On order over $100</span>
+        </div>
+      </div>
+      {/* Елемент з іконкою */}
+      <div className="servises__item">
+        <div className="servises__item__icon bg-color-2">
+          <div className="servises__item__icon__circle">
+            <IconVegetable width="50" height="50" color="#fff" color1="#fff" />
+          </div>
+        </div>
+        <div className="servises__item__body">
+          <h3 className="servises__item__body__heading">Always Fresh</h3>
+          <span>Product well package</span>
+        </div>
+      </div>
+      {/* Елемент з іконкою */}
+      <div className="servises__item">
+        <div className="servises__item__icon bg-color-3">
+          <div className="servises__item__icon__circle">
+            <IconSuperiorQuality
+              width="60"
+              height="60"
+              color="#fff"
+              color1="#fff"
+            />
+          </div>
+        </div>
+        <div className="servises__item__body">
+          <h3 className="servises__item__body__heading">Superior Quality</h3>
+          <span>Quality Products</span>
+        </div>
+      </div>
+      {/* Елемент з іконкою */}
+      <div className="servises__item">
+        <div className="servises__item__icon bg-color-4">
+          <div className="servises__item__icon__circle">
+            <IconSupport
+              width="60"
+              height="60"
+              color="#fff"
+              color1="#fff"
+              color2="#fff"
+              color3="#fff"
+              color4="#fff"
+            />
+          </div>
+        </div>
+        <div className="servises__item__body">
+          <h3 className="servises__item__body__heading">Support</h3>
+          <span>24/7 Support</span>
+        </div>
+      </div>
+
+      <style jsx>{`
+        .servises__section {
+          padding: 90px 2vw;
+          //position: relative;
+          display: flex;
+          flex-direction: row; //В рядок
+          flex-wrap: wrap;
+          //justify-content: space-evenly; //Рівномірно,з країв  1/2 .
+          justify-content: space-around; //Рівномірно всюди .
+          //align-items: center; //Y-вертикально //R-щоб рівняло по верху
+        }
+
+        .servises__item {
+          display: flex;
+          flex-direction: column; //в стовбець;
+          //justify-content: center; //Тут по Y //R-щоб рівняло по верху
+          align-items: center; //Y але тут по X
+          flex: 1 1 auto;
+          width: 600px;
+          margin: 20px 0;
+        }
+        .servises__item__icon {
+          display: flex;
+          padding: 2px;
+          justify-content: center; //X-горизонтально
+          align-items: center; //Y-вертикально
+          text-align: center; //X-для тексту(не flex)
+          //flex: 1 0 110px;
+          flex-basis: 100px; //R-щоб не розтягувало коли servises__item/align-items: center;
+          min-width: 100px;
+          min-height: 100px;
+          border-radius: 50%;
+        }
+        .servises__item__icon__circle {
+          display: flex;
+          padding: 2px;
+          justify-content: center; //X-горизонтально
+          align-items: center; //Y-вертикально
+          text-align: center; //X-для тексту(не flex)
+          //flex: 1 0 80px;
+          min-width: 80px; //
+          min-height: 80px;
+          border-radius: 50%;
+          border: 1px solid #fff; /* Параметры границы */
+        }
+        .servises__item__icon:hover {
+          background: #82ae46;
+        }
+        .bg-color-1 {
+          background: #e4b2d6;
+        }
+
+        .bg-color-2 {
+          background: #dcc698;
+        }
+
+        .bg-color-3 {
+          background: #a2d1e1;
+        }
+
+        .bg-color-4 {
+          background: #dcd691;
+        }
+        .servises__item__body {
+          text-transform: uppercase;
+          font-size: 12px;
+          font-weight: 500;
+          color: gray;
+        }
+        .servises__item__body__heading {
+          margin: 0 0 8px;
+          text-transform: uppercase;
+          font-size: 15px;
+          line-height: 1.8;
+          font-weight: 500;
+          color: #000000;
+        }
+
+        //@media (min-width: 768px) {
+        @media (min-width: 600px) {
+          //планшети
+          .servises__section {
+            padding: 90px 5vw;
+          }
+          .servises__item {
+            width: calc((100%) / 2);
+            margin: 0 0 24px;
+            padding: 0 0;
+          }
+        }
+        //@media (min-width: 992px) {
+        @media (min-width: 960px) {
+          //нетбуки деякі планшети i >
+          .servises__item {
+            width: calc((100% - 40px) / 4);
+          }
+        }
+      `}</style>
+    </section>
+  );
+};
+export default Services;
--- pages/[lang]/index--.js
+++ pages/[lang]/index--.js
@@ -1,19 +1,14 @@
-//HomePage.js / Muiv4.5.1
+//home_mui.js / Muiv4.5.1
 //Добавлено animat
 import React, { useContext } from "react";
 import Link from "next/link";
 import Head from "next/head";
+
 import Layout from "../../components/all/Layout";
 import useTranslation from "../../translations/useTranslation";
 import { ComponentContext } from "../../context/ComponentContext";
-import Servises from "../../components/mag/homePage/Services";
-import Сategory from "../../components/mag/homePage/Сategory";
-import Products from "../../components/mag/homePage/Products";
-// import Сategory from "./category";
 
-const HomePage = () => {
-  // export default function HomePage() {
-  const disabled = false; //Для buton
+export default function HomePage() {
   const { locale, t } = useTranslation();
   const { state } = useContext(ComponentContext);
   const theme = state.theme;
@@ -22,10 +17,13 @@ const HomePage = () => {
     // <AppFrame classes={{ drawer: className.drawer }}>
     <Layout>
       <Head>
-        {/* <title>{t("header-titleSite")}</title> */}
-        <title>mag1</title>
+        <title>{t("header-titleSite")}</title>
+        {/* <title>Ra-test</title> */}
       </Head>
-      <section className="home-slider-section">
+      {/* Домашня стор/ hero-???*/}
+      {/* <section className="hero"> */}
+      {/* слайдер *** owl-carousel=text-align: center; */}
+      <section className="home-slider">
         {/* елемент слайдеру */}
         <div className="slider-item image1">
           {/* overlay/перекривати-Робить тінь */}
@@ -35,18 +33,11 @@ const HomePage = () => {
             <h2 className="subheading">
               Ми постачаємо органічні овочі та фрукти
             </h2>
-            <Link
-              //ніби посилання на Докладніше
-              href={`/[lang]/examples/flexbox`}
-              as={`/${locale}/examples/flexbox`}
-            >
-              <a
-                className="button"
-                // onClick={buttonClick1}
-              >
-                Докладніше/flexbox
+            <p>
+              <a href="#" class="btn btn-primary">
+                Докладніше
               </a>
-            </Link>
+            </p>
           </div>
         </div>
         {/* елемент слайдеру */}
@@ -54,68 +45,49 @@ const HomePage = () => {
           {/* overlay/перекривати-Робить тінь */}
           <div className="overlay"></div>
           <div className="slider-text">
+            {/* <h1>100% Fresh &amp; Organic Foods</h1> */}
             <h1>100% свіжа та органічна їжа</h1>
             <h2 className="subheading">
               Ми постачаємо органічні овочі та фрукти
             </h2>
-            {/* <a href="#" className="button" onClick={buttonClick2}> */}
-            <a
-              href={`/[lang]/examples/grid`}
-              as={`/${locale}/examples/grid`}
-              className="button"
-              // onClick={buttonClick2}
-            >
-              Докладніше/grid
-            </a>
-            <div></div>
+            <p>
+              <a href="#" class="btn btn-primary">
+                Докладніше
+              </a>
+            </p>
           </div>
         </div>
       </section>
-      {/*--Servises  */}
-      <Servises />
-      {/*--Сategory */}
-      <Сategory />
-      {/*--Products  */}
-      <Products />
       <style jsx>{`
-      .home-slider-section {
+        .home-slider {
           position: relative;
           height: 650px;
-        }
-        .slider-text h1 {
-          font-size: 40px;
-          color: white;
-        }
-        @media (min-width: 960px) {
-          .slider-text h1 {
-            font-size: 8vw;
-          color: white;
-          }
+          z-index: -9;
         }
         .image2 {
           background: 100% transparent url(/vegefoods/images/bg_1.jpg) no-repeat;
-          z-index: 10;
         }
         .image1 {
           background: 100% transparent url(/vegefoods/images/bg_2.jpg) no-repeat;
           animation-delay: 5s; //Затримка анімації для 2-го слайду
-          z-index: 10;
         }
         .slider-item {
           position: absolute; //Щоб не бачити цей <div> !!! Інакше 1-й слайд буде відкириватись за 1-м
-          //position: relative; //1-й слайд буде відкириватись за 1-м
           //0,0,0,0-розтягує position: absolute; на весь батьківський блок не залежно від контенту
-          //top: 0;
+          top: 0;
           bottom: 0;
           left: 0;
           right: 0;
           align-items:center;
           justify-content:center;
-          height: 650px;
-          background-size: cover; //Маштабує зображення.
-          background-repeat: no-repeat;
-          background-position: center center;
+          height: 650px; //ш
+          background-size: cover; //ш-Маштабує зображення.
+          background-repeat: no-repeat; //-ш
+          background-position: center center; //ш
+          //z-index: 0;
+          //
           animation-name: coverSlide; //ім'я секції анімації
+          //animation-fill-mode: forwards; //rЗалишається в кінцевому стані*/
           animation-timing-function: linear; //рівномірна зміна
           animation-iteration-count: infinite; //к-сть повторів/rinfinite нескінченно
           animation-duration: 5s; //протяжність анімації
@@ -136,13 +108,10 @@ const HomePage = () => {
             opacity: 0;
           }
         from {
-            //transform: scale(1.05);
-          z-index:1  //Для видимості button
-
+            transform: scale(1.05);
           }
           to {
-            //transform: scale(1);
-            z-index:0 //Для видимості button
+            transform: scale(1);
           }
         }
 
@@ -160,12 +129,12 @@ const HomePage = () => {
 
         .slider-text {
           //position: absolute;//З аbsolute не працює центрування
-          padding: 0 15px;
           display: flex;
           align-items: center; //Y-вертикально
           justify-content: center; //X-горизонтально
           flex-direction: column; //в стовбець
           text-align: center;
+          //
           height: 650px;//Треба
         }
 
@@ -179,50 +148,21 @@ const HomePage = () => {
           color: white;
         }
         .slider-text h1 {
-          font-size: 40px
-          //font-size: 8vw;
+          font-size: 8vw;
           color: white;
           line-height: 1.3;
-          font-weight: 200;
+          font-weight: 100;
           font-family: ${theme.fontFamily.mimicHand};
-          //font-family: ${
-            theme.fontFamily.notoSans
-          };//!!! не працює/Локальний шрифт
-          //font-family: Noto Sans;//Працює!!! Загрузка локального шрифта через <style jsx global> Loyout.js
+          //font-family: ${theme.fontFamily.notoSans};//!!! не працює/Локальний шрифт
+          /*font-family: Noto Sans;//Працює!!! Загрузка локального шрифта через <style jsx global> Loyout.js*/
+          //@include media-breakpoint-down(sm) {font-size: 40px;color: $pri; }
         }
 
         .slider-text p {
           color: rgba(0, 0, 0, 0.8);
           font-weight: 400;
         }
-        .button {
-          display: fllex-line; //щоб не переносилась і ширина по контексту
-          position: relative;
-          padding: 6px 12px;
-          //background: #70b4e6;
-          background: #82ae46;
-          //width: "";
-          height: "40зч";
-          color: #fff;
-          border: 2px solid #82ae46;
-          border-radius: 20px;
-          //font-family: "Poppins,Geneva, Arial, sans-serif";
-          //font-family: ${theme.fontFamily.sansSerif};
-           font-size: 16px;
-          //font-weight: "";
-          //opacity: 1;
-          //list-style-type: none;
-          text-decoration: none;
-          //z-index: 100;
-        }
-        .button:hover {
-          background-color:rgba(132,177,71,.3);
-          border: 1px solid #fff;
-          //border: 1px solid rgba(42,66,10,1);
-          // cursor: ${disabled ? "not-allowed" : "pointer"};
-        }
       `}</style>
     </Layout>
   );
-};
-export default HomePage;
+}
